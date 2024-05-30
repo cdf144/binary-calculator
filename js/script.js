f38510fa342349442c874943238727346a638f62
Trần Đình Phú <56244176+cdf144@users.noreply.github.com>
2024-05-30 12:07:33 +0700

    feat(js): revamp parsing and validating functions
    This commit resolves/processes many errors concerning invalid inputs, and
    also add documentation for relevant, non-obvious functions.

--- js/script.js
+++ js/script.js
@@ -9,30 +9,69 @@ let ops = [];
 
 /**
  * Parse the expression and store the numbers and operators in separate arrays
- * @throws {Error} If the expression is invalid (contains redundant operators)
+ * @returns {boolean} true if the expression is parsed successfully and need to be
+ * computed, false otherwise
  */
 function parseExpression() {
   const expr = resultText.textContent;
-  nums = expr.split(/[\+\-\*\/]/).filter((num) => num !== "").map((num) =>
-    parseInt(num, 2)
-  );
-  ops = expr.match(/[\+\-\*\/]/g);
-  if (ops.length !== nums.length - 1) {
-    throw new Error("Invalid expression (redundant operators are not allowed)");
+  if (!expr) {
+    return false;
   }
+
+  const numsMatches = expr.match(/\d+/g);
+  nums = numsMatches ? numsMatches.map((num) => parseInt(num, 2)) : [];
+
+  ops = expr.match(/[\+\-\*\/]/g) || [];
+  if (nums.length === 1 && ops.length === 0) {
+    return false;
+  }
+  if (nums.length === 0 && ops.length > 0) {
+    alert("Syntax error: missing numbers");
+    return false;
+  }
+
+  try {
+    checkValidity(expr, nums, ops);
+  } catch (e) {
+    alert(e.message);
+    return false;
+  }
+
+  return true;
+}
+
+function checkValidity(expr, nums, ops) {
+  const startsWithOperator = /^[\+\-\*\/]/.test(expr);
+  if (startsWithOperator) {
+    if (!(["+", "-"].includes(ops[0]))) {
+      throw new Error(
+        "Syntax error: only + and - operators are allowed at the beginning",
+      );
+    }
+    updateFirstNumber(nums, ops);
+  }
+
+  if (ops.length !== nums.length - 1) {
+    throw new Error(
+      "Syntax error: there may be redundant operators or missing numbers",
+    );
+  }
+}
+
+/**
+ * Updates the first number in the array by adding the shifted operator to it.
+ * @param {number[]} nums - The array of numbers.
+ * @param {string[]} ops - The array of operators.
+ */
+function updateFirstNumber(nums, ops) {
+  nums[0] = Number(ops.shift() + nums[0]);
 }
 
 /**
  * Calculate the result of the expression
  */
 function calculate() {
-  try {
-    parseExpression();
-  } catch (e) {
-    alert(e.message);
-  }
-
-  if (nums.length < 2) {
+  if (parseExpression() === false) {
     return;
   }
   calculateMulDiv();
